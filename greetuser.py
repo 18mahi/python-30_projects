b1a8a50c71a4512d69301150d9f12157000a9238
mahi jindal <127783778+18mahi@users.noreply.github.com>
2025-06-14 17:39:42 +0530

    Add files via upload

---
+++ greetuser.py
@@ -0,0 +1,5 @@
+name=input("enter your name: ")
+print(f"Hello, {name}!")
+# This code prompts the user to enter their name and then greets them with a message.
+# The code uses an f-string to format the greeting message.
+# The input function is used to get the user's name, and the print function displays the greeting.
